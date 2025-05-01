1b94b8c06ced77c8adb46ddcf1877cafb9f3d41e
fcb2b21 <b2b21@fcb2b.co.kr>
2025-05-01 17:59:28 +0900

    빌드 때 env 적용

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# 1) 런타임 .env에서 값을 읽어 config.js 생성
-cat <<EOF > /usr/share/nginx/html/config.js
-window.__CONFIG__ = {
-  VITE_SERVER_ROUTE: "${VITE_SERVER_ROUTE}"
-};
-EOF
-# 2) Nginx 실행
-exec nginx -g 'daemon off;'
